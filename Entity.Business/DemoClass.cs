64d6515f427aec12f59446fda3a18ddac47fbb03
Ritik Anubhav <Ritik.Anubhav@consilio.com>
2024-08-17 13:53:58 +0530

    data annotations

--- Entity.Business/DemoClass.cs
+++ Entity.Business/DemoClass.cs
@@ -1,12 +1,16 @@
-﻿namespace Entity.Business
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entity.Business
 {
 	public class DemoClass
 	{
 		//add id for a primary key( prefix can be class name but Id must be at the last)
+		[Required]
 		public int DemoClassId { get; set; }
-
+		[Required]
 		public int Age { get; set; }
-
+		[Required]
+		[StringLength(50,ErrorMessage ="characters > 50")]
 		public string Name { get; set; }
 	}
 }
